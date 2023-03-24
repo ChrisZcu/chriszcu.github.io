8d1e6712ea0fb3425d9931bb9c1f9315fd026655
ChrisZcu <1164806828@qq.com>
2023-03-24 20:06:22 +0800

    add

--- scripts/config_xml.js
+++ scripts/config_xml.js
@@ -2,8 +2,8 @@ var TSC = TSC || {};
 
 TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
    <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
-      <rdf:Description dc:date="2023-03-24 04:05:55 上午" dc:source="Camtasia,9.1.5,enu" dc:title="v2 no voice" tscDM:firstFrame="v2_no_voice_First_Frame.png" tscDM:originId="0A4ACD62-3DBC-41B9-88FA-42DC90CA2CC7" tscDM:project="v1 no voice">\
-         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="204400"/>\
+      <rdf:Description dc:date="2023-03-24 08:02:18 下午" dc:source="Camtasia,9.1.5,enu" dc:title="v1 no voice" tscDM:firstFrame="v1_no_voice_First_Frame.png" tscDM:originId="0A4ACD62-3DBC-41B9-88FA-42DC90CA2CC7" tscDM:project="v1 no voice">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="285233"/>\
          <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="1080" stDim:w="1920"/>\
          <tsc:langName>\
             <rdf:Bag>\
@@ -29,7 +29,7 @@ TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta
             <rdf:Description>\
                <tscDM:files>\
                   <rdf:Seq>\
-                     <rdf:li xmpDM:name="0" xmpDM:value="v2 no voice.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="v2_no_voice_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="v2_no_voice_Thumbnails.png"/></rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="v1 no voice.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="v1_no_voice_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="v1_no_voice_Thumbnails.png"/></rdf:Seq>\
                </tscDM:files>\
             </rdf:Description>\
          </tscDM:contentList>\
